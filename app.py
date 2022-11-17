242a6c2d238f05afa08ccc5d1ec8fafc8cad0bb7
Kartik <34774250+kartiknawani@users.noreply.github.com>
2022-11-18 00:37:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -134,7 +134,7 @@ if filename is not None:
                     st.plotly_chart(analysis.chatter(data))
                     
                     st.write('**Sentiment analysis:**')
-                    sanalysis.sentiments(data)
+                    analysis.sentiments(data)
                     
                 else:
                     member_data = data[data['Author'] == member]
